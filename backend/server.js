10239d50c5ce32ec90c03bf832a5ee1f8f7f485c
AkshithaSharma-gif <akshithasharma682@gmail.com>
2026-09-02 14:14:12 +0530

    Setup Express server and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Test route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "MedAssist API is running successfully",
+  });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`MedAssist server running on port ${PORT}`);
+});
